c3dd22e335d3a166670d3e993fecdf836acab94a
immortal521 <immort521@gmail.com>
2025-08-26 19:39:03 +0800

    chore: move config to root
    - Add .gitignore for node_modules
    
    - Move husky and commitlint to root
    
    - Add pre-commit hook with lint-staged
    
    - Consolidate config in root package.json

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,6 +5,10 @@ export default {
     "type-empty": [2, "never"],
     "subject-empty": [2, "never"],
     "header-max-length": [2, "always", 100],
-    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "chore"]],
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "chore"],
+    ],
   },
 };
