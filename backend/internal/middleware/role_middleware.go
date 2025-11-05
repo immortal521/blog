617ad80de8e895ca56af8c7d18891d572cffafe7
immortal521 <immort521@gmail.com>
2025-11-05 19:33:03 +0800

    fix: improve role middleware error handling
    - Remove unused errors import
    
    - Check all errors instead of specific user not found error

--- backend/internal/middleware/role_middleware.go
+++ backend/internal/middleware/role_middleware.go
@@ -4,7 +4,6 @@ import (
 	"blog-server/internal/entity"
 	"blog-server/internal/service"
 	"blog-server/pkg/errs"
-	"errors"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -30,11 +29,10 @@ func (r *RoleMiddleware) Handler(roles ...entity.UserRole) fiber.Handler {
 			return errs.New(errs.CodeUnauthorized, "missing user uuid", nil)
 		}
 		ok, err := r.authService.HasRole(c.UserContext(), userUUID)
-		if errors.Is(err, errs.ErrUserNotFound) || !ok {
+		if err != nil || !ok {
 			return errs.New(errs.CodeUnauthorized, "user not found", err)
 		}
 
 		return c.Next()
 	}
-
 }
