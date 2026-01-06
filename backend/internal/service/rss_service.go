51a6c7673b2887eb9541096ef4db20c1bcf3dbea
immortal521 <immort521@gmail.com>
2026-01-06 16:41:36 +0800

    refactor: reorder imports in rss_service.go
    - Group internal packages together
    
    - Improve import organization for clarity

--- backend/internal/service/rss_service.go
+++ backend/internal/service/rss_service.go
@@ -1,16 +1,15 @@
 package service
 
 import (
+	"blog-server/internal/config"
+	"blog-server/internal/database"
+	"blog-server/internal/repo"
+	"blog-server/internal/response"
 	"context"
 	"encoding/xml"
 	"sort"
 	"strconv"
 	"time"
-
-	"blog-server/internal/config"
-	"blog-server/internal/database"
-	"blog-server/internal/repo"
-	"blog-server/internal/response"
 )
 
 type IRssService interface {
