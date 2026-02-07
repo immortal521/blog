53d504450aef2a59f5891bdb221459c068b2312c
immortal521 <immort521@gmail.com>
2026-02-07 21:25:11 +0800

    feat: add adminSubLayout component
    - Create new layout with full viewport size
    
    - Apply scoped styles with Less for text color

---
+++ frontend/app/layouts/adminSubLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="admin-sub-layout"></div>
+</template>
+
+<style lang="less" scoped>
+.admin-sub-layout {
+  width: 100vw;
+  height: 100vh;
+  color: var(--text-color-base);
+}
+</style>
