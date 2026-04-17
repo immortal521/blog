0354b2edce9ce26e16defc6c252da3aed08ee500
immortal521 <immort521@gmail.com>
2026-04-17 18:19:41 +0800

    feat: add ent schemas for core entities
    - Add User, Post, Comment, Link, LinkCategory schemas
    
    - Add PostCategory, PostTag, and their relation schemas
    
    - Introduce TimeMixin for common timestamp fields
    
    - Setup entity relationships and constraints
    
    - Add ent generate command in backend/ent/generate.go

---
+++ backend/ent/schema/link.go
@@ -0,0 +1,64 @@
+package schema
+
+import (
+	"blog-server/entity"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// Link holds the schema definition for the Link entity.
+type Link struct {
+	ent.Schema
+}
+
+func (Link) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		TimeMixin{},
+	}
+}
+
+// Fields of the Link.
+func (Link) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("description").
+			MaxLen(255).
+			Optional(),
+
+		field.Bool("enabled").
+			Default(false),
+
+		field.String("name").
+			MaxLen(100),
+
+		field.Int("sort_order").
+			Default(0),
+
+		field.String("url").
+			MaxLen(255).
+			Unique(),
+
+		field.String("avatar").
+			MaxLen(255).
+			Optional(),
+
+		field.Enum("status").
+			GoType(entity.LinkStatus("")).
+			Default(string(entity.LinkStatusNormal)),
+
+		field.Uint("category_id").
+			Optional(),
+	}
+}
+
+// Edges of the Link.
+func (Link) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.
+			From("category", LinkCategory.Type).
+			Ref("links").
+			Unique().
+			Field("category_id"),
+	}
+}
---
+++ backend/ent/schema/post.go
@@ -0,0 +1,72 @@
+package schema
+
+import (
+	"blog-server/entity"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// Post holds the schema definition for the Post entity.
+type Post struct {
+	ent.Schema
+}
+
+func (Post) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		TimeMixin{},
+	}
+}
+
+// Fields of the Post.
+func (Post) Fields() []ent.Field {
+	return []ent.Field{
+		field.Uint("user_id"),
+
+		field.String("title").
+			MaxLen(255),
+
+		field.String("summary").
+			MaxLen(255).
+			Optional().
+			Nillable(),
+
+		field.Text("content"),
+
+		field.String("cover").
+			MaxLen(255).
+			Optional().
+			Nillable(),
+
+		field.Uint("read_time_minutes"),
+
+		field.Uint("view_count").
+			Default(0),
+
+		field.Enum("status").
+			GoType(entity.PostStatus("")).
+			Default(string(entity.PostStatusArchived)),
+
+		field.Time("published_at").
+			Optional().
+			Nillable(),
+	}
+}
+
+// Edges of the Post.
+func (Post) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("author", User.Type).
+			Ref("posts").
+			Field("user_id").
+			Unique().
+			Required(),
+
+		edge.To("categories", PostCategory.Type).
+			Through("post_category_relations", PostCategoryRelation.Type),
+
+		edge.To("tags", PostTag.Type).
+			Through("post_tag_relations", PostTagRelation.Type),
+	}
+}
