08f76b37b8d7657f3fb6ad840580c9ff2272f287
immortal521 <immort521@gmail.com>
2026-04-18 03:05:43 +0800

    refactor: remove GORM tags and TableName methods from entities
    - Strip primary key and deleted at tags from Comment and LinkCategory
    
    - Remove TableName methods for default table naming
    
    - Simplify struct fields by removing constraints and indexes

--- backend/entity/link_category.go
+++ backend/entity/link_category.go
@@ -3,15 +3,10 @@ package entity
 import "time"
 
 type LinkCategory struct {
-	ID        uint `gorm:"primarykey"`
+	ID        uint
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	DeletedAt *time.Time `gorm:"type:timestamptz;index"`
 
-	Name      string `gorm:"column:name;size:20;not null;unique"`
-	SortOrder int    `gorm:"not null;default:0"`
-}
-
-func (*LinkCategory) TableName() string {
-	return "link_categories"
+	Name      string
+	SortOrder int
 }
