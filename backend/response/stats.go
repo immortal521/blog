ae9059cf8a11ad4468a1fd69540e9d2370a57544
immortal521 <immort521@gmail.com>
2026-09-14 18:12:31 +0800

    chore: remove dashboard stats response

--- backend/response/stats.go
+++
@@ -1,8 +0,0 @@
-package response
-
-type DashboardStatsRes struct {
-	TotalPV       int64 `json:"totalPV"`
-	TotalUV       int64 `json:"totalUV"`
-	TotalComments int64 `json:"totalComments"`
-	TotalPosts    int64 `json:"totalPosts"`
-}
